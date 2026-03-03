42e1d46162d145860d28cef31d27b3b7290130c9
missionStrawberry <annam7245@sd61learn.ca>
2026-03-02 23:30:37 -0500

    undo change to config.ts

--- clean-architecture-visualizer/frontend/src/i18n/config.ts
+++ clean-architecture-visualizer/frontend/src/i18n/config.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enChecker from './locales/en/checker.json' with { type: 'json' };
+import enChecker from './locales/en/checker.json';
 
 i18n.use(initReactI18next).init({
   resources: { en: { checker: enChecker } },
