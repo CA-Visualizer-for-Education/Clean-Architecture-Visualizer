93c8c0e7b26f44e4ac8227ef3203c8a6aabc13f3
hiuyear <hiuyankwok002@gmail.com>
2026-06-09 15:41:20 -0400

    feat: add getFilesWithViolations use case files (WIP)

---
+++ clean-architecture-visualizer/src/use_case/getFilesWithViolations/getFilesWithViolationsOutputData.ts
@@ -0,0 +1,15 @@
+export class GetFilesWithViolationsOutputData {
+    
+    private filesWithViolationsOutputData?: { [key: string]: any }[];
+
+    // called by interactor to store result
+    setOutputData(OutputData: {[key: string]: any }[]) {
+        this.filesWithViolationsOutputData = OutputData;
+    }
+
+    // called by presenter
+    getOutputData(): object {
+        if (this.filesWithViolationsOutputData) return this.filesWithViolationsOutputData;
+        return {};
+    }
+}
