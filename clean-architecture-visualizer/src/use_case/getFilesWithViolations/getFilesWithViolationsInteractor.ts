960fb3e0afa8f11c440c0fc251509f3400ba41dd
hiuyear <hiuyankwok002@gmail.com>
2026-06-10 15:13:43 -0400

    bug: fixed naming errors

--- clean-architecture-visualizer/src/use_case/getFilesWithViolations/getFilesWithViolationsInteractor.ts
+++ clean-architecture-visualizer/src/use_case/getFilesWithViolations/getFilesWithViolationsInteractor.ts
@@ -2,9 +2,8 @@ import type { GetFilesWithViolationsInputBoundary} from "./getFilesWithViolation
 import type { SessionDBAccessInterface } from "../../data_access/sessionDBAccessInterface.js";
 import type { GetFilesWithViolationsOutputData } from "./getFilesWithViolationsOutputData.js";
 
-export class GetFilesWithViolationInteractor implements GetFilesWithViolationsInputBoundary {
+export class GetFilesWithViolationsInteractor implements GetFilesWithViolationsInputBoundary {
     
-    // new per session. construcing REQUIRES input of BOTH DBA and Output data (blank)
     constructor (
         private readonly db: SessionDBAccessInterface,
         private readonly outputdata: GetFilesWithViolationsOutputData,
@@ -12,19 +11,14 @@ export class GetFilesWithViolationInteractor implements GetFilesWithViolationsIn
 
     async execute(): Promise<void> {
 
-         // - getNodesByStatus("VIOLATION")
         const violatingNodes = this.db.getNodesByStatus("VIOLATION");
 
-        // - map to filePaths
         const violatingNodesPaths = violatingNodes.map((node) => node.filePath);
         
-        // - remove nodes with undefined filepatrhs
         const filteredViolatingNodePaths = violatingNodesPaths.filter((path): path is string => path != undefined)
 
-        // - deduplicate with Set
         const uniqueNodesPaths = [...new Set(filteredViolatingNodePaths)];
 
-        // - setOutputData({ total_violations, files })
         this.outputdata.setOutputData({ total_violations: violatingNodes.length, files: uniqueNodesPaths})
     }
 }
