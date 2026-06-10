8ffad065013488ff511171f792cb966c2649b3b5
hiuyear <hiuyankwok002@gmail.com>
2026-06-10 14:27:29 -0400

    presenter

---
+++ clean-architecture-visualizer/src/interface_adapter/getFilesWithViolations/getFilesWithViolationsPresenter.ts
@@ -0,0 +1,13 @@
+import type {GetFilesWithViolationsOutputBoundary} from "../../use_case/getFilesWithViolations/getFilesWithViolationsOutputBoundary.js"
+import type { GetFilesWithViolationsOutputData } from "../../use_case/getFilesWithViolations/getFilesWithViolationsOutputData.js"
+
+export class GetFilesWithViolationsPresenter implements GetFilesWithViolationsOutputBoundary {
+
+    constructor(
+        private readonly outputData: GetFilesWithViolationsOutputData
+    ){}
+
+    getOutputData(){
+        return this.outputData.getOutputData();
+    }
+}
