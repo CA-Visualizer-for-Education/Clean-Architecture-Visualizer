507083d700323f37022a04f6d87090a94bcbe679
hiuyear <hiuyankwok002@gmail.com>
2026-07-07 12:15:43 -0400

    refactor: remove redundant comments from getFilesWithViolations output data

--- clean-architecture-visualizer/src/use_case/getFilesWithViolations/getFilesWithViolationsOutputData.ts
+++ clean-architecture-visualizer/src/use_case/getFilesWithViolations/getFilesWithViolationsOutputData.ts
@@ -2,12 +2,10 @@ export class GetFilesWithViolationsOutputData {
     
     private filesWithViolationsOutputData?: { [key: string]: any };
 
-    // called by interactor to store result
     setOutputData(OutputData: {[key: string]: any }) {
         this.filesWithViolationsOutputData = OutputData;
     }
 
-    // called by presenter
     getOutputData(): object {
         if (this.filesWithViolationsOutputData) return this.filesWithViolationsOutputData;
         return {};
